48c2bc85da15cc89e402f90ed643627d9a7664dc
jeffreysternberg530 <32045436+jeffreysternberg530@users.noreply.github.com>
2018-11-28 13:23:31 -0500

    update, full image analysis pipeline with image cluster printouts

--- full_image_analysis.py
+++ full_image_analysis.py
@@ -25,7 +25,7 @@ img = np.array(img)
 
 m,n = 209,247
 
-sklearn_pca = PCA(n_components=30)
+sklearn_pca = PCA(n_components=30, random_state=4)
 Y_sklearn = sklearn_pca.fit_transform(img)
 
 import pandas as pd
@@ -63,19 +63,49 @@ plt.show()
 
 print(df.sort_values(by= ['cluster','image'], ascending=True))
 
-
-cluster = df[df["cluster"]==1].sort_values(by= 'image', ascending=True)
-print(cluster)
-cluster_1 = cluster['image'].tolist()
-print(cluster_1)
-
-import matplotlib.image as mpimg
-
-graphics= []
-for image in cluster_1:
-    graphics.append(io.imread("image_samples_resized/" +image))
+def cluster_lister(df, column):
+    
+    cluster_list = []
+    k = df[column].max()
+    x = df[column].min()
+    
+    k=k+1
+    
+    for i in range(x,k):
+    
+        cluster_i= df[df[column]==i].sort_values(by= 'image', ascending=True)
+        cluster_i_list = cluster_i['image'].tolist()
+        cluster_list.append(cluster_i_list)
+        
+    return cluster_list
+    
+def image_lister(cluster_list):
+    
+    image_list = []
+    k = len(cluster_list)
+    
+    for i in range(k):
+        img_i = []
+        for image in cluster_list[i]:
+            img_i.append(io.imread("image_samples_resized/" +image))
+            
+        image_list.append(img_i)
+            
+    return image_list
     
 def show_images(images, cols = 1, titles = None):
+    """Display a list of images in a single figure with matplotlib.
+    
+    Parameters
+    ---------
+    images: List of np.arrays compatible with plt.imshow.
+    
+    cols (Default = 1): Number of columns in figure (number of rows is 
+                        set to np.ceil(n_images/float(cols))).
+    
+    titles: List of titles corresponding to each image. Must have
+            the same length as titles.
+    """
     assert((titles is None)or (len(images) == len(titles)))
     n_images = len(images)
     if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
@@ -89,4 +119,17 @@ def show_images(images, cols = 1, titles = None):
     fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
     plt.show()
     
-show_images(graphics, cols = 3, titles = cluster_1)
+def image_cluster_viewer(image_list, cluster_list):
+    
+    k = len(image_list)
+    
+    for i in range(k):
+        show_images(image_list[i], cols = 3, titles = cluster_list[i])
+
+        
+cluster_list = cluster_lister(df, 'cluster')
+image_list = image_lister(cluster_list)
+image_cluster_viewer(image_list, cluster_list)
+
+
+
